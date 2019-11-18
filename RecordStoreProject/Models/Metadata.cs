01f0ba33a3bd5cc4161782056e01ed3e1e32a257
John Young <johnfyoung22@yahoo.com>
2019-11-18 15:09:20 -0600

    Part I
    Created website, controllers, and database. Added Metadata and PartialClasses.

---
+++ RecordStoreProject/Models/Metadata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecordStoreProject.Models
+{
+    public class GenreMetadata
+    {
+        [StringLength(50)]
+        [Required(ErrorMessage = "A genre is required")]
+        [Display(Name = "Genre name")]
+        public string GenreName;
+    }
+    public class ArtistMetadata
+    {
+        [StringLength(100)]
+        [Required(ErrorMessage = "An artist name is required")]
+        [Display(Name = "Artist name")]
+        public string ArtistName;
+    }
+    public class AlbumMetadata
+    {
+        [StringLength(100)]
+        [Required(ErrorMessage = "An album name is required")]
+        [Display(Name = "Album name")]
+        public string AlbumName;
+
+        [Range(1, 100, ErrorMessage = "Album price must be between 1 and 100")]
+        [Display(Name = "Album Price")]
+        public decimal AlbumPrice;
+    }
+}
---
+++ RecordStoreProject/Models/PartialClasses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecordStoreProject.Models
+{
+    public class PartialClasses
+    {
+        [MetadataType(typeof(GenreMetadata))]
+        public partial class Genre
+        {
+
+        }
+
+        [MetadataType(typeof(ArtistMetadata))]
+        public partial class Artist
+        {
+
+        }
+
+        [MetadataType(typeof(AlbumMetadata))]
+        public partial class Album
+        {
+
+        }
+    }
+}
